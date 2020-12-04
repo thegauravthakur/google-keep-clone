e24366d4c62d680fe1fa28dfdf1738a5a9a55c91
Gaurav Thakur <gthakur581@gmail.com>
2020-12-04 09:33:49 +0530

    Added some meta tags and favicon

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useRecoilState } from "recoil";
 import { notesState } from "./recoil/atoms";
 import app from "./api/firebase";
 import { Footer } from "./components/Footer";
+import MetaTags from "react-meta-tags";
 
 function App() {
   const [notes, setNotes] = useRecoilState(notesState);
@@ -25,21 +26,32 @@ function App() {
   }, []);
 
   return (
-    <div className="flex min-h-screen flex-col">
-      <h1 className="text-center font-bold text-3xl py-7">Google Keep Clone</h1>
-      <CustomInputField />
-      <div className="flex-1 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-5 sm:mx-14  md:mx-24 gap-5 py-7 ">
-        {notes.map((note, index) => (
-          <Card
-            index={index}
-            key={note.id}
-            title={note.title}
-            body={note.body}
-          />
-        ))}
+    <Fragment>
+      <MetaTags>
+        <title>Google Keep Clone</title>
+        <meta
+          name="description"
+          content="Simple google keep clone made with reactjs and firebase"
+        />
+      </MetaTags>
+      <div className="flex min-h-screen flex-col">
+        <h1 className="text-center font-bold text-3xl py-7">
+          Google Keep Clone
+        </h1>
+        <CustomInputField />
+        <div className="flex-1 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-5 sm:mx-14  md:mx-24 gap-5 py-7 ">
+          {notes.map((note, index) => (
+            <Card
+              index={index}
+              key={note.id}
+              title={note.title}
+              body={note.body}
+            />
+          ))}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Fragment>
   );
 }
 
